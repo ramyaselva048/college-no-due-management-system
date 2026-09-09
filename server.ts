8bd1e9a1dce251e2774aef05080ba76ecfde3afc
ramyaselva048 <ramyaselva048@gmail.com>
2026-09-09 12:29:15 +0530

    Updated server.ts

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { createServer as createViteServer } from 'vite';
 import { apiRouter } from './server/routes';
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 async function startServer() {
   const app = express();
